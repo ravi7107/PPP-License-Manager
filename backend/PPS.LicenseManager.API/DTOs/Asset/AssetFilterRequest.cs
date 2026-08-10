06d467f14711e2f5de5ddc58bd29aeae93751eee
ravi7107 <r.r.shete@gmail.com>
2026-08-10 06:02:33 +0000

    Add AssetFilterRequest DTO

---
+++ backend/PPS.LicenseManager.API/DTOs/Asset/AssetFilterRequest.cs
@@ -0,0 +1,24 @@
+namespace PPS.LicenseManager.API.DTOs.Asset;
+
+public class AssetFilterRequest
+{
+    public string? Search { get; set; }
+
+    public int? DepartmentId { get; set; }
+
+    public string? AssetType { get; set; }
+
+    public string? Manufacturer { get; set; }
+
+    public string? Status { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public int Page { get; set; } = 1;
+
+    public int PageSize { get; set; } = 20;
+
+    public string SortBy { get; set; } = "AssetTag";
+
+    public string SortDirection { get; set; } = "asc";
+}
